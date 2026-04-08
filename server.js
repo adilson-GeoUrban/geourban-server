7687b9b8cefaae7359a78dfdc0a085284a8ef682
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-08 16:53:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-require("dotenv").config();
-
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
-// Liberar arquivos da pasta public
-app.use(express.static("public"));
-
-// Rota principal
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
 app.listen(PORT, () => {
-  console.log("Servidor rodando");
+  console.log("Servidor rodando na porta " + PORT);
 });
