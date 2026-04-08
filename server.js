f4422543bfb8865267615191f228069ecf9aabea
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-08 15:35:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require("dotenv").config();
+const IP_PERMITIDO = "127.0.0.1";
+const ip = req.ip;
+const IP_PERMITIDO = "127.0.0.1";
+
+app.use((req, res, next) => {
+    const ip = req.ip.replace("::ffff:", "");
+
+    if (ip !== IP_PERMITIDO) {
+        return res.status(403).send("🚫 Acesso restrito");
+    }
+
+    next();
+});
