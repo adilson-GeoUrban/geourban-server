aa652f28c08847001ed6941f9aea562ee3bd48dc
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-08 16:34:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-// 🔐 IP permitido (somente você)
-const IP_PERMITIDO = "127.0.0.1";
-
-app.use((req, res, next) => {
-    const ip = req.ip.replace("::ffff:", "");
-
-    if (ip !== IP_PERMITIDO) {
-        return res.status(403).send("🚫 Acesso restrito");
-    }
-
-    next();
-});
-
-// 🌐 Porta obrigatória do Render
 const PORT = process.env.PORT || 3000;
 
+// Liberar arquivos da pasta public
+app.use(express.static("public"));
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(PORT, () => {
-    console.log("Servidor rodando");
+  console.log("Servidor rodando");
 });
