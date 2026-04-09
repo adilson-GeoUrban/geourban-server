83d780df903b6e3b9dc5d4552583bb232b5be647
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-09 00:22:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-node server.js
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// SERVIR PUBLIC
+app.use(express.static(path.join(__dirname, "public")));
+
+// ROTA PRINCIPAL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
