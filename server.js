c201a8c3be3fd440b1a50013a6ab196259dcaa81
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-08 23:46:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-"PMTBf#Xk29!pL@2026$GeoUrban";
+const fs = require("fs");
+
+// caminho dos arquivos importantes
+const arquivos = [
+    "./server.js",
+    "./public/index.html",
+    "./logs.txt",
+    "./backup.json"
+];
+
+// pasta do ponto de restauração
+const ponto = "./backup_pre_chave";
+
+// criar pasta se não existir
+if (!fs.existsSync(ponto)) fs.mkdirSync(ponto);
+
+// copiar arquivos
+arquivos.forEach(file => {
+    if (fs.existsSync(file)) {
+        const nome = file.split("/").pop();
+        fs.copyFileSync(file, `${ponto}/${nome}`);
+    }
+});
+
+console.log("✅ Ponto de restauração criado com sucesso!");
