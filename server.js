621c22e0533e49efbca5cae41a30d8e1e2b66110
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-09 21:15:56 -0300

    fix: restaurar servidor e index.htmlUpdate server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// permitir JSON
+app.use(express.json());
+
+// servir arquivos da pasta public
+app.use(express.static(path.join(__dirname, "public")));
+
+// rota principal
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// start servidor
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
+});
