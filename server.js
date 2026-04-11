ec58f55abbc55406471915712525f167ab7939cc
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-11 15:12:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 🔵 servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+// 🔵 CAMINHO ABSOLUTO CORRETO
+const publicPath = path.join(__dirname, 'public');
 
-// 🔵 rota principal
+// 🔵 SERVIR ARQUIVOS
+app.use(express.static(publicPath));
+
+// 🔵 INDEX
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-// 🔵 rota login
+// 🔵 LOGIN
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(publicPath, 'login.html'));
+});
+
+// 🔴 DEBUG (IMPORTANTE)
+app.get('/teste', (req, res) => {
+  res.send("SERVIDOR OK");
 });
 
 app.listen(PORT, () => {
