0b784f940a40732c703a4f9657afbb37afb7da27
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-11 12:01:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 node server.js
+process.on("uncaughtException", (err) => {
+  console.error("ERRO CRÍTICO:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("PROMISE NÃO TRATADA:", err);
+});
