f80e930ce0aea29e7fe6ea0b5923c12563efb7ee
adilson-GeoUrban <adilson@deltopografia.com>
2026-04-12 09:22:08 -0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
-if (data.acao === "REDIRECT") {
-  window.open(data.destino, "_blank");
+function adicionarMensagemNaTela(msg) {
+  const chat = document.getElementById("chat");
+  const div = document.createElement("div");
+  div.innerText = msg;
+  chat.appendChild(div);
+}
+
+async function enviar() {
+  const input = document.getElementById("input").value;
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ comando: input })
+  });
+
+  const data = await res.json();
+
+  adicionarMensagemNaTela("Luiza: " + data.mensagem);
+
+  if (data.acao === "REDIRECT") {
+    window.location.href = data.destino;
+  }
 }
